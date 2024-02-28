9c7207f99e792670becf5f158f7777823fd83496
SunJinxu <sunjinxu1996@gmail.com>
2024-02-29 00:25:56 +0800

    [SQL COMPILER AND VM] 框架搭建

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # 24SimpleDB
 a simple db inspired by sqlite
 
-## 1 Making a Simple REPL(read-execute-print loop)
+## 1. Making a Simple REPL(read-execute-print loop)
 关键点在与实现三个主要组件：
 * prompt打印
 * 控制台输入消息接收
 * 消息接收处理逻辑
 
 其中，prompt打印可以打印我们自己的`24SimpleDb`标识；控制台消息接收使用自定义的结构体进行读取和存储；消息处理逻辑则用if_else结构进行匹配。
+
+## 2. SQL Compiler and Virtual Machine
+SQL中的command分为两种: 一种为`meta-commands`, 另一种为`普通command`
+* `meta-commands`  Non-SQL中的概念，通常以`.`开头; 表示需要系统对这些命令进行处理
+* `普通command` 普通command，是需要SQL Compile解析，并由VM执行的命令
+  * SQL Compile: 将command解析为Statement结构(模拟bytecode)
+  * Virtural Machine: 执行Statement(bytecode)
+
+`Statement` 为可以被VM执行的结构，实际DB中为bytecode。其中含有`type`类型表示其类型
--- src/main.c
+++ src/main.c
@@ -7,9 +7,29 @@
 #define false 0
 #define DB_EXIT_SIGN ".exit"
 
-void printPrompt() {
-    printf(DB_PROMPT);
-}
+/**
+ * meta_commands返回值枚举类
+*/
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+/**
+ * 准备sql语句的返回值枚举类
+*/
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+/**
+ * Statement类型枚举
+*/
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
 
 /**
  * 信息缓存结构体
@@ -19,6 +39,13 @@ typedef struct InputBuffer {
     size_t bufferSize;  // 内存长度（用于getline函数出参）
     ssize_t inputSize;  // 有效长度
 } InputBuffer;
+
+/**
+ * 打印提示消息
+*/
+void printPrompt() {
+    printf(DB_PROMPT);
+}
 
 /**
  * 创建并返回一个信息缓存指针
@@ -53,6 +80,53 @@ void readCliInput(InputBuffer *inputBuffer) {
     inputBuffer->buffer[readCliLen - 1] = '\0';
 }
 
+/**
+ * 处理meta-commands
+*/
+MetaCommandResult executeMetaCommand(InputBuffer *InputBuffer) {
+    if (strcmp(InputBuffer->buffer, ".exit") == 0) {
+        exit(EXIT_SUCCESS);
+    }
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+}
+
+/**
+ * Statement结构体
+*/
+typedef struct {
+    StatementType statementType;
+} Statement;
+
+/**
+ * Statement处理方法
+*/
+PrepareResult prepareStatement(InputBuffer *inputBuffer, Statement *statement) {
+    if (strncmp(inputBuffer->buffer, "insert", 6) == 0) {
+        statement->statementType = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    } else if (strcmp(inputBuffer->buffer, "select") == 0) {
+        statement->statementType = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+/**
+ * Statement执行方法
+*/
+void executeStatement(Statement *statement) {
+    switch (statement->statementType) {
+        case (STATEMENT_INSERT):
+            printf("This is a insert statement\n");
+            break;
+        case (STATEMENT_SELECT):
+            printf("This is a select statement\n");
+            break;
+        default:
+            break;
+    }
+}
+
 int main() {
     InputBuffer *userInput = createInputBuffer();
     printf("welcome to use 24SimpleDb!\n");
@@ -61,11 +135,29 @@ int main() {
         printPrompt();
         readCliInput(userInput);
 
-        if (strcmp(userInput->buffer, DB_EXIT_SIGN) == 0) {
-            printf("24SimpleDb stopped!");
-            exit(EXIT_SUCCESS);
-        } else {
-            printf("unrecognized command '%s'. \n", userInput->buffer);
+        // 处理meta_command
+        if (userInput->buffer[0] == '.') {
+            MetaCommandResult metaRet = executeMetaCommand(userInput);
+            switch (metaRet) {
+                case META_COMMAND_SUCCESS:
+                    break;
+                case META_COMMAND_UNRECOGNIZED_COMMAND:
+                    printf("Unrecognized command '%s'\n", userInput->buffer);
+                    break;
+            }
+            continue;
+        }
+        
+        // 处理statement
+        Statement statement;
+        PrepareResult prepareRet = prepareStatement(userInput, &statement);
+        switch (prepareRet) {
+            case PREPARE_SUCCESS:
+                executeStatement(&statement);
+                break;
+            case PREPARE_UNRECOGNIZED_STATEMENT:
+                printf("Prepare statement failed '%s'\n", userInput->buffer);
+                break;
         }
     }
 
